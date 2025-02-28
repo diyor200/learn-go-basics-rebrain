e8d7c6cd70c347ee9b2dfbfb81698f2b113134d4
d.abdulaxatov <d.abdulaxatov@hamkorbank.uz>
2025-02-28 14:12:47 +0500

    section 7: with pool testing

--- bool_test.go
+++ bool_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 type Counter struct {
 	A int
@@ -12,8 +15,22 @@ func (c *Counter) Inc() {
 	c.B++
 }
 
-func BenchmarkCounter_Inc(b *testing.B) {
-	counter := &Counter{}
+//func BenchmarkCounter_Inc(b *testing.B) {
+//	counter := &Counter{}
+//	for i := 0; i < b.N; i++ {
+//		for j := 0; j < 10000; j++ {
+//			b.StopTimer()
+//			counter.Inc()
+//			b.StartTimer()
+//		}
+//	}
+//}
+
+func BenchmarkCounter_WithPool(b *testing.B) {
+	var counterPool = sync.Pool{
+		New: func() interface{} { return Counter{} },
+	}
+	counter := counterPool.Get().(Counter)
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < 10000; j++ {
 			b.StopTimer()
