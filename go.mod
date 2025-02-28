24749246cb2911f7fce1cdeadf3e3089e8f43b76
d.abdulaxatov <d.abdulaxatov@hamkorbank.uz>
2025-02-28 16:30:36 +0500

    section 8: using errorgroup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_basics_rebrain
 
 go 1.24.0
+
+require golang.org/x/sync v0.11.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,75 @@
-package learn_go_basics_rebrain
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	//fmt.Println(runTasks())
+	fmt.Println(runTasksWithErrorGroup())
+}
+
+func runTasksWithErrorGroup() error {
+	var (
+		eg    = new(errgroup.Group)
+		tasks = []func() error{
+			func() error { time.Sleep(time.Second * 2); return errors.New("task 1 fail") },
+			func() error { time.Sleep(time.Second * 1); return errors.New("task 2 fail") },
+			func() error { time.Sleep(time.Second * 3); return errors.New("task 3 fail") },
+		}
+	)
+
+	for _, task := range tasks {
+		t := task
+		eg.Go(func() error {
+			return t()
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+// without sync.errorGroup
+//func runTasks() error {
+//	var (
+//		wg      sync.WaitGroup
+//		errChan = make(chan error, 1)
+//		tasks   = []func() error{
+//			func() error { time.Sleep(time.Second * 2); return errors.New("task 1 fail") },
+//			func() error { time.Sleep(time.Second * 1); return errors.New("task 2 fail") },
+//			func() error { time.Sleep(time.Second * 3); return errors.New("task 3 fail") },
+//		}
+//	)
+//
+//	for _, task := range tasks {
+//		wg.Add(1)
+//		go func(t func() error) {
+//			defer wg.Done()
+//			if err := t(); err != nil {
+//				select {
+//				case errChan <- err:
+//				default:
+//					fmt.Println("can't write error to errChan, because it is full! err:", err)
+//				}
+//			}
+//		}(task)
+//	}
+//
+//	wg.Wait()
+//	close(errChan)
+//
+//	if err, ok := <-errChan; ok {
+//		return err
+//	}
+//
+//	return nil
+//}
